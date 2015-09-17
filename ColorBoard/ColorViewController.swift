7787408f2e5786c95d89262913376fa118ffa132
jroland <james.a.roland@gmail.com>
2015-09-17 13:45:58 -0700

    Added support for dynamic background color

--- ColorBoard/ColorViewController.swift
+++ ColorBoard/ColorViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class ColorViewController: UIViewController {
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var textField: UITextField!
+    
     @IBAction func dismiss(sender: AnyObject) {
         presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    @IBAction func changeColor(sender: AnyObject) {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        let newColor = UIColor( red: red, green: green, blue: blue, alpha: 1)
+        view.backgroundColor = newColor
     }
 }
